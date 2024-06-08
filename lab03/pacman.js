4633a5a7f29418116a76dd07e3ae59627c6e8b37
Lola-chk <61632476+Lola-chk@users.noreply.github.com>
2024-06-07 23:04:25 -0400

    Create game board

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -1,3 +1,28 @@
 function createGame(n){
-    
+    let gameArray = new Array(n).fill(".");
+
+    // Get a random unoccupied spot
+    function getRandomEmptyIndex(excludeIndices) {
+        let index;
+        do {
+            index = Math.floor(Math.random() * n);
+        } while (excludeIndices.includes(index));
+        return index;
+    }
+
+    // Place Pacman ("C")
+    const pacmanIndex = getRandomEmptyIndex([]);
+    gameArray[pacmanIndex] = "C";
+
+    // Place Ghost ("^")
+    const ghostIndex = getRandomEmptyIndex([pacmanIndex]);
+    gameArray[ghostIndex] = "^";
+
+    // Place fruit ("@")
+    const fruitIndex = getRandomEmptyIndex([pacmanIndex, ghostIndex]);
+    gameArray[fruitIndex] = "@";
+
+    return gameArray;
 }
+
+console.log(createGame(10));
